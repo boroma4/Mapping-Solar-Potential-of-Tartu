98fda686548abc7ef7fdb8dcd5cc9d3786074476
Bohdan Romashchenko <bromashchenko@twilio.com>
2022-06-05 16:05:12 +0300

    fix typos

--- scripts/linux/tartu.sh
+++ scripts/linux/tartu.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 
-python3.9 pipeline/main.py --lod 2 --filename delta.gml --pv-efficiency 0.21 --pv-loss 0.14 --roof-coverage 0.90 --optimize-2d true --output-format tiles
+python3.9 pipeline/main.py --lod 2 --filename tartu.gml --pv-efficiency 0.21 --pv-loss 0.14 --roof-coverage 0.90 --optimize-2d true --output-format tiles
