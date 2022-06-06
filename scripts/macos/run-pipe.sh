2ff89ed024d6bed77d9c55ee88fff10ad3b87d5e
Bohdan Romashchenko <bromashchenko@twilio.com>
2022-06-06 21:50:58 +0300

    scripts..

--- scripts/macos/run-pipe.sh
+++ scripts/macos/run-pipe.sh
@@ -4,4 +4,6 @@ FILENAME=$1
 
 python3 pipeline/main.py --lod 2 --filename $FILENAME --pv-efficiency 0.21 --pv-loss 0.14 --roof-coverage 0.90 --optimize-2d true --output-format tiles
 
+echo "Removing files from web-app/public and copying results there"
+rm web-app/public/*
 cp data/lod2/tartu-output/* web-app/public/
