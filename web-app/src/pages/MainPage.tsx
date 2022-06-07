9a4f52baf37f655a6c8d1cb971a5cdbae1d5ad55
Bohdan Romashchenko <bromashchenko@twilio.com>
2022-06-07 19:50:56 +0300

    fix typo

--- web-app/src/pages/MainPage.tsx
+++ web-app/src/pages/MainPage.tsx
@@ -9,8 +9,8 @@ interface Props {
 export default function MainPage({cities, setCity}: Props) {
   return (
     <div className={"center-content"} style={{backgroundColor: "#C2B280"}}>
-        <h2> Welcome to the Project Solar X</h2>
-        <h5>Pleace choose a city</h5>
+        <h2> Estonian Solar Potential Project</h2>
+        <h5> Please choose a city </h5>
         <CitySelector cities={cities} setCity={setCity} />
     </div>
   );
