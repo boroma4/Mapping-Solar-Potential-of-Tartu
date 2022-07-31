12f95354a0c739e35437311f51ac3920fa76e35c
Bohdan Romashchenko <bromashchenko@twilio.com>
2022-07-31 17:36:50 +0300

    fix web app preprocessing race condition

--- web-app/scripts/preprocessor.mjs
+++ web-app/scripts/preprocessor.mjs
@@ -1,6 +1,8 @@
 import { readdirSync, writeFileSync } from "fs";
 import { exec } from "child_process";
 
+const sleep = async(ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const getSubDirectoryNames = (source) =>
   readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
@@ -13,15 +15,22 @@ const writeMetadata = () => {
   writeFileSync('cities/meta.json', data);
 }
 
-const moveOutputsFromData = () => {
+const moveOutputsFromData = async() => {
   const outputDirectories = getSubDirectoryNames("../data/lod2").filter((name) => name.endsWith("-output"));
+  let processedOutputs = 0;
+
   for (const name of outputDirectories) {
     exec(`"scripts/move_data.sh" ${name.replace("-output", "")}`, (error, out) => {
       console.log(out);
+      processedOutputs += 1;
       if (error) {
         console.error(error);
       }
     });
+  }
+
+  while(processedOutputs < outputDirectories.length){
+    await sleep(500);
   }
 }
 
