1363dbd3fc9419437ed93874e92b5618c2961bb4
Bohdan Romashchenko <bromashchenko@twilio.com>
2022-08-02 18:20:20 +0300

    Revert "update vite config"
    This reverts commit 043f593255fb3b30aa389a4e962dde896f90e6c1.

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -4,10 +4,5 @@ import cesium from 'vite-plugin-cesium';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cesium()],
-  server: {
-    hmr: {
-      port: 443
-    }
-  }
+  plugins: [react(), cesium()]
 })
